f44ea9b12eccfeab1a46e2599cde54f5a951c871
unknown <Achaqilal407@gmail.com>
2025-01-13 23:00:52 +0800

    clear set up DB

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import router from "./router/route.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
+const databaseUri = process.env.DATABASE;
+
+const app = express();
+app.use(express.json({extended: true})); 
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api', router);
+
+mongoose
+    .connect(databaseUri, { 
+        
+    })
+    .then(() => console.log("Connected to the database"))
+    .catch(err => console.error("Failed to connect to the database:", err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
