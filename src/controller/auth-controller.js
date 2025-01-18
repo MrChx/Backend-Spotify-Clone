7d75da0567f9ede223a5515f3f5c745b60c735cb
unknown <Achaqilal407@gmail.com>
2025-01-18 18:50:13 +0800

    uncomplete auth

---
+++ src/controller/auth-controller.js
@@ -0,0 +1,53 @@
+import { User } from "../model/user-model.js";
+
+export const authCallback = async (req, res, next) => {
+	try {
+		const { id, firstName, lastName, imageUrl } = req.body;
+
+		// check if user already exists
+		const user = await User.findOne({ clerkId: id });
+
+		if (!user) {
+			// signup
+			await User.create({
+				clerkId: id,
+				fullName: `${firstName || ""} ${lastName || ""}`.trim(),
+				imageUrl,
+			});
+		}
+
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.log("Error in auth callback", error);
+		next(error);
+	}
+};
+
+export const getAllUsers = async (req, res, next) => {
+	try {
+		const currentUserId = req.auth.userId;
+		const users = await User.find({ clerkId: { $ne: currentUserId } });
+		res.status(200).json(users);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const getMessages = async (req, res, next) => {
+	try {
+		const myId = req.auth.userId;
+		const { userId } = req.params;
+
+		const messages = await Message.find({
+			$or: [
+				{ senderId: userId, receiverId: myId },
+				{ senderId: myId, receiverId: userId },
+			],
+		}).sort({ createdAt: 1 });
+
+		res.status(200).json(messages);
+	} catch (error) {
+		next(error);
+	}
+};
+
--- src/router/route.js
+++ src/router/route.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { protectRoute, requireAdmin } from "../middleware/authMiddleware.js";
+import { authCallback, getAllUsers, getMessages } from "../controller/auth-controller.js";
 
 const router = express.Router();
+router.use(protectRoute, requireAdmin);
+
+router.route('/callback').post(authCallback);
+router.route('/getUser').get(protectRoute, getAllUsers);
+router.route('/message/:userId').get(protectRoute, getMessages);
 
 export default router;
